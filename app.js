8887e859f2013cb1321983d6168444e379009e6a
Ismael Rivera <Maelo104@warhorse-m2.lan>
2024-06-06 19:30:37 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to the world of your making.'));
+app.get('/', (req, res) => res.send('Welcome to the world of your making. Hold on tight.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
